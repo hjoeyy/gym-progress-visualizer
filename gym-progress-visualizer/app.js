3374e99ed451bb021a2c6e490ae56fa8be8ba1b0
hjoeyy <90590862+hjoeyy@users.noreply.github.com>
2025-07-04 17:25:04 -0700

    gym progress visualizer
    just playing with chart js

--- gym-progress-visualizer/app.js
+++ gym-progress-visualizer/app.js
@@ -1,3 +1,5 @@
+
+
 const re = /^(?:\d{2})([/])\d{2}\1\d{4}$/;
 
 function testDate(dateInput) {
@@ -409,3 +411,27 @@ displayTotalWorkouts(workouts, totalWorkoutsLogged);
 displayMostImprovedLift(workouts, mostImprovedExercise);
 displayWeekStreak(workouts, bestWeekStreak);
 displayTotalProgressIncrease(workouts, totalProgressIncrease);
+
+
+// chart
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
